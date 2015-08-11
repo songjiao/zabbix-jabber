bf3276e718500a55cb5b636b95b7c4ad2e804524
songjiao <songjiao@cyou-inc.com>
2015-08-11 20:22:09 +0800

    first commit

--- send_kwai.sh
+++ send_kwai.sh
@@ -3,4 +3,4 @@ echo $(date) >> /usr/local/zabbix/share/zabbix/alertscripts/alarm.log
 echo $1 >> /usr/local/zabbix/share/zabbix/alertscripts/alarm.log
 echo $2 >> /usr/local/zabbix/share/zabbix/alertscripts/alarm.log
 echo $3 >> /usr/local/zabbix/share/zabbix/alertscripts/alarm.log
-/usr/local/zabbix/share/zabbix/alertscripts/send_client.py  -j alarm@test.com -p moc.iawkmi -t alarm@conference.test.com -m "$3"
+/usr/local/zabbix/share/zabbix/alertscripts/send_client.py  -j alarm@test.com -p password -t alarm@conference.test.com -m "$3"
